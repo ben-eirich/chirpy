0e216540fc0e6084247e5dc1b3cac59ee4eeccf9
Ben Eirich <beirich@gmail.com>
2024-09-11 23:27:17 -0400

    1.11 Custom Handlers

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	fmt.Println("Hello world")
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzHandler)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -19,4 +20,10 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func healthzHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
 }
