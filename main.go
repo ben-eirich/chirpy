2e4076e311d759e872a18b19cb6cfa061d9074ff
Ben Eirich <beirich@gmail.com>
2024-09-17 23:01:04 -0400

    4.2 validate_chirp
    4.2: cleanup with helper funcs

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
@@ -15,6 +17,8 @@ func main() {
 	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerAdminMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handleValidateChirp)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -71,4 +75,46 @@ func (cfg *apiConfig) handlerAdminMetrics(w http.ResponseWriter, _ *http.Request
 	w.WriteHeader(200)
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(fmt.Sprintf(template, cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handleValidateChirp(w http.ResponseWriter, req *http.Request) {
+	type params struct {
+		Body string `json:"body"`
+	}
+	type retValue struct {
+		Valid bool `json:"valid"`
+	}
+
+	apiParams := params{}
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&apiParams)
+	if err != nil {
+		respondWithError(w, 500, "Error decoding request")
+	} else if len(apiParams.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+	} else {
+		respondWithJSON(w, 200, retValue{Valid: true})
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type retError struct {
+		Error string `json:"error"`
+	}
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	eo, _ := json.Marshal(retError{Error: msg})
+	w.Write(eo)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	resp, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+	} else {
+		w.WriteHeader(code)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(resp)
+	}
 }
